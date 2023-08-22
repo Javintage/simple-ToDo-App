da42fb35be7dacd7eb3dea0f0892765357de4150
javintage <martin.timona@gmail.com>
2023-08-22 12:36:20 +0300

    Second commit

--- Public/browser.js
+++ Public/browser.js
@@ -1,10 +1,48 @@
+ function Itemlist(item){
+    return `<li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
+    <span class="item-text">${item.text}</span>
+    <div>
+      <button  data-id="${item._id}"class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
+      <button data-id="${item._id}" class="delete-me btn btn-danger btn-sm">Delete</button>
+    </div>
+  </li>`
+ }
+//create feature
+let createField=document.getElementById("create-field")
+document.getElementById("create-form").document.addEventListener("submit",function(e){
+    e.preventDefault()
+    axios.post('/create-item',{text: createField.value}).then(function(response){
+        // create the HTML for  a new item
+        document.getElementById("item-list").insertAdjacentHTML("beforeend",Itemlist(response.data))
+        createField.value=""
+        createField.focus()
+     } ).catch(
+        console.log("Please Try again later")
+    )
+})
+
 document.addEventListener("click",function(e){
+    //Delete Feature
+    if(e.target.classList.contains("delete-me")){
+        if(confirm('Do you really want to delete this item permanently?')){
+            axios.post('/delete-item',{id: e.target.getAttribute(data-id)}).then(function(){
+                e.target.parentElement.parentElement.remove()
+             }).catch(function(){
+                console.log("Please Try again later")
+              })
+
+        }
+    }
+    //Update Feature
     if(e.target.classList.contains("edit-me")){
-        let UserInput=prompt("Enter your new text")
-        axios.post('/update item',{Text :UserInput}).then(
-            // do something
-        ).catch(
+        let UserInput=prompt("Enter your new text",e.target.parentElement.parentElement.querySelector(".item-text").innerHTML)
+      if(UserInput){
+        axios.post('/update-item',{Text :UserInput,id: e.target.getAttribute(data-id)}).then(function
+            (){
+            e.target.parentElement.parentElement.querySelector(".item-text").innerHTML=UserInput
+             } ).catch(
             console.log("Please Try again later")
         )
+      }
     }
 })
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 let express =require("express")
-let {MongoClient}=require('mongodb')
+let {MongoClient,ObjectId}=require('mongodb')
 let App = express()
 App.use(express.static('Public'))
 let db
@@ -10,6 +10,7 @@ async function go(){
   App.listen(3000)
 }
 go()
+App.use(express.json())
 App.use(express.urlencoded({extended:false}))
 App.get('/',function(req,res){
   db.collection("items").find().toArray(function(err,items){
@@ -26,28 +27,29 @@ App.get('/',function(req,res){
         <h1 class="display-4 text-center py-1">To-Do App</h1>
         
         <div class="jumbotron p-3 shadow-sm">
-          <form action="/create-item" method="POST">
+          <form  id ="create-form"action="/create-item" method="POST">
             <div class="d-flex align-items-center">
-              <input name="item" autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;">
+              <input id="create-field" name="item" autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;">
               <button class="btn btn-primary">Add New Item</button>
             </div>
           </form>
         </div>
         
-        <ul class="list-group pb-5">
+        <ul  id ="item-list" class="list-group pb-5">
         
-          ${items.map(function(){
-            return `  <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
+          ${items.map(function(item){
+            return `<li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
             <span class="item-text">${item.text}</span>
             <div>
-              <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
-              <button class="delete-me btn btn-danger btn-sm">Delete</button>
+              <button  data-id="${item._id}"class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
+              <button data-id="${item._id}" class="delete-me btn btn-danger btn-sm">Delete</button>
             </div>
           </li>`
           }).join('')}
         </ul>
         
       </div>
+      <script src ="https://unpkg.com/axios/dist/axios.min.js"></script>
       <script src ="/browser.js"></script>
     </body>
     </html>`)
@@ -55,8 +57,20 @@ App.get('/',function(req,res){
    
 })
 App.post('/create-item',function(req,res){
-    db.collection("items").insertOne({text:req.body.item},function(){
-        res.redirect('/')
-    })
+    db.collection("items").insertOne({text :req.body.text},function(err,info){
+      res.json({_id :info.insertId,text :req.body.text})
+        
+    }) 
 })
+App.post('/update-item',function(req,res){
+  db.collection('items').findOneandUpdate({_id : new ObjectId(req.body.id)},{$set :{ text : req.body.text}},function(){
+    res.send("Success")
+  })
+})
+App.post('/delete-item',function(req,res){
+  db.collection('items').deleteOne({_id : new ObjectId(req.body.id)},function(){
+    res.send("Deleted Successfully")
+  })
+})
+
 
